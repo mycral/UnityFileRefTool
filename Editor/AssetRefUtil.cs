c744f278bfc802e19b8123be4c17d6cae4a36401
mycral <guoliulong@gmail.com>
2025-03-23 18:27:49 +0800

    makepakcage

--- Editor/AssetRefUtil.cs
+++ Editor/AssetRefUtil.cs
@@ -4,95 +4,101 @@ using UnityEngine;
 using UnityEditor;
 using System.Linq;
 
-public class AssetRefUtil
+
+namespace AssetRefTool
 {
-    public class UnityFileData
+
+    public class AssetRefUtil
     {
-        public string m_kPath;
-        public HashSet<UnityFileData> m_kDepends = new HashSet<UnityFileData>();
-        public HashSet<UnityFileData> m_kRefeds = new HashSet<UnityFileData>();
-
-        public void Clear()
+        public class UnityFileData
         {
-            m_kDepends.Clear();
-            m_kRefeds.Clear();
-        }
-    }
+            public string m_kPath;
+            public HashSet<UnityFileData> m_kDepends = new HashSet<UnityFileData>();
+            public HashSet<UnityFileData> m_kRefeds = new HashSet<UnityFileData>();
 
-    public static HashSet<UnityFileData> m_kSelectedUnityFileData = new HashSet<UnityFileData>();
-    public static Dictionary<string,UnityFileData> m_kAllUnityFileDataDic = new Dictionary<string,UnityFileData>();
-    public static HashSet<UnityFileData> m_kRetDependsUnityFileData = new HashSet<UnityFileData>();
-
-    public static void FindFolderRefs(string[] folderPaths)
-    {
-        m_kAllUnityFileDataDic.Clear();
-        m_kRetDependsUnityFileData.Clear();
-        m_kSelectedUnityFileData.Clear();
-
-        HashSet<string> allContainsFilePaths = new HashSet<string>(1024);
-        List<string> srcFolders = new List<string>();
-        foreach (string path in folderPaths)
-        {
-            if(AssetDatabase.IsValidFolder(path))
+            public void Clear()
             {
-                srcFolders.Add(path);
-            }
-            else
-            {
-                allContainsFilePaths.Add(path);
+                m_kDepends.Clear();
+                m_kRefeds.Clear();
             }
         }
 
-        string[] allFolderFileGuids =  AssetDatabase.FindAssets("", srcFolders.ToArray());
-        HashSet<string> allContainsFileGuids = new HashSet<string>(1024);
-        foreach(var folderGuid in allFolderFileGuids)
+        public static HashSet<UnityFileData> m_kSelectedUnityFileData = new HashSet<UnityFileData>();
+        public static Dictionary<string, UnityFileData> m_kAllUnityFileDataDic = new Dictionary<string, UnityFileData>();
+        public static HashSet<UnityFileData> m_kRetDependsUnityFileData = new HashSet<UnityFileData>();
+
+        public static void FindFolderRefs(string[] folderPaths)
         {
-            allContainsFileGuids.Add(folderGuid);
+            m_kAllUnityFileDataDic.Clear();
+            m_kRetDependsUnityFileData.Clear();
+            m_kSelectedUnityFileData.Clear();
+
+            HashSet<string> allContainsFilePaths = new HashSet<string>(1024);
+            List<string> srcFolders = new List<string>();
+            foreach (string path in folderPaths)
+            {
+                if (AssetDatabase.IsValidFolder(path))
+                {
+                    srcFolders.Add(path);
+                }
+                else
+                {
+                    allContainsFilePaths.Add(path);
+                }
+            }
+
+            string[] allFolderFileGuids = AssetDatabase.FindAssets("", srcFolders.ToArray());
+            HashSet<string> allContainsFileGuids = new HashSet<string>(1024);
+            foreach (var folderGuid in allFolderFileGuids)
+            {
+                allContainsFileGuids.Add(folderGuid);
+            }
+
+
+            foreach (var guid in allContainsFileGuids)
+            {
+                allContainsFilePaths.Add(AssetDatabase.GUIDToAssetPath(guid));
+            }
+
+            //下面这个foreach 会包含所有相关的对象
+            foreach (var refFilePath in AssetDatabase.GetDependencies(allContainsFilePaths.ToArray(), true))
+            {
+                UnityFileData fileData = new UnityFileData() { m_kPath = refFilePath };
+                if (allContainsFilePaths.Contains(refFilePath))
+                {
+                    m_kSelectedUnityFileData.Add(fileData);
+                }
+                else
+                {
+                    m_kRetDependsUnityFileData.Add(fileData);
+                }
+
+                if (!m_kAllUnityFileDataDic.ContainsKey(refFilePath))
+                {
+                    m_kAllUnityFileDataDic.Add(refFilePath, fileData);
+                }
+                else
+                {
+                    m_kAllUnityFileDataDic[refFilePath].Clear();
+                }
+            }
+            //接下来只需要梳理关系即可
+            foreach (var fileKv in m_kAllUnityFileDataDic)
+            {
+                var refFiles = AssetDatabase.GetDependencies(fileKv.Key, true);
+                UnityFileData curFileData = fileKv.Value;
+                foreach (var refFile in refFiles)
+                {
+                    if (refFile == curFileData.m_kPath)
+                        continue;
+
+                    var refFileData = m_kAllUnityFileDataDic[refFile];
+                    curFileData.m_kDepends.Add(refFileData);
+                    refFileData.m_kRefeds.Add(curFileData);
+                }
+            }
+
         }
-
-
-        foreach (var guid in allContainsFileGuids)
-        {
-            allContainsFilePaths.Add(AssetDatabase.GUIDToAssetPath(guid));
-        }
-        
-        //下面这个foreach 会包含所有相关的对象
-        foreach (var refFilePath in AssetDatabase.GetDependencies(allContainsFilePaths.ToArray(),true))
-        {
-            UnityFileData fileData = new UnityFileData() { m_kPath = refFilePath };
-            if(allContainsFilePaths.Contains(refFilePath))
-            {
-                m_kSelectedUnityFileData.Add(fileData);
-            }
-            else
-            {
-                m_kRetDependsUnityFileData.Add(fileData);
-            }
-
-            if(!m_kAllUnityFileDataDic.ContainsKey(refFilePath))
-            {
-                m_kAllUnityFileDataDic.Add(refFilePath, fileData);
-            }
-            else
-            {
-                m_kAllUnityFileDataDic[refFilePath].Clear();
-            }
-        }
-        //接下来只需要梳理关系即可
-        foreach (var fileKv in m_kAllUnityFileDataDic)
-        {
-            var refFiles = AssetDatabase.GetDependencies(fileKv.Key, true);
-            UnityFileData curFileData = fileKv.Value;
-            foreach (var refFile in refFiles)
-            {
-                if (refFile == curFileData.m_kPath)
-                    continue;
-
-                var refFileData =  m_kAllUnityFileDataDic[refFile];
-                curFileData.m_kDepends.Add(refFileData);
-                refFileData.m_kRefeds.Add(curFileData);
-            }
-        }
-
     }
 }
+
--- Editor/AssetRefWindow.cs
+++ Editor/AssetRefWindow.cs
@@ -4,207 +4,212 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.IMGUI.Controls;
 
-public class AssetRefWindow : EditorWindow
+namespace AssetRefTool
 {
-    [MenuItem("Assets/FindFolderRefs")]
-    public static void CreateWindow()
+
+    public class AssetRefWindow : EditorWindow
     {
-        GetWindow<AssetRefWindow>().Show();
-        
-    }
-
-    static string[] m_kLastGUIDs;
-
-    private List<string> m_kSelectFolderPaths = new List<string>();
-    private List<string> m_kRefFilePaths = new List<string>();
-
-    private Vector2 m_kScrollPos;
-    private Vector2 m_kScrollPos1;
-    private Vector2 m_kScrollPos2;
-    private Vector2 m_kScrollPos3;
-    private SearchField m_kSearchField;
-    private string m_kCurSearchText;
-
-    private AssetRefUtil.UnityFileData m_kCurSelectUnityFileData;
-
-    private float m_fNumberWidth = 50;
-
-
-    private static bool AreArraysEqual(string[] a, string[] b)
-    {
-        if (a == null || b == null || a.Length != b.Length) return false;
-        for (int i = 0; i < a.Length; i++)
+        [MenuItem("Assets/FindFolderRefs")]
+        public static void CreateWindow()
         {
-            if (a[i] != b[i]) return false;
-        }
-        return true;
-    }
+            GetWindow<AssetRefWindow>().Show();
 
-
-    private void OnEnable()
-    {
-        if(m_kSearchField == null)
-        {
-            m_kSearchField = new SearchField();
         }
 
+        static string[] m_kLastGUIDs;
 
-        EditorApplication.update -= Update;
-        EditorApplication.update += Update;
-    }
+        private List<string> m_kSelectFolderPaths = new List<string>();
+        private List<string> m_kRefFilePaths = new List<string>();
 
-    private void OnDisable()
-    {
-        EditorApplication.update -= Update;
-    }
+        private Vector2 m_kScrollPos;
+        private Vector2 m_kScrollPos1;
+        private Vector2 m_kScrollPos2;
+        private Vector2 m_kScrollPos3;
+        private SearchField m_kSearchField;
+        private string m_kCurSearchText;
+
+        private AssetRefUtil.UnityFileData m_kCurSelectUnityFileData;
+
+        private float m_fNumberWidth = 50;
 
 
-    private void _onSelectionChange()
-    {
-        m_kCurSelectUnityFileData = null;
-        m_kRefFilePaths.Clear();
-        m_kSelectFolderPaths.Clear();
-
-        var curFolderGuids = Selection.assetGUIDs;
-        foreach (var folder in curFolderGuids)
+        private static bool AreArraysEqual(string[] a, string[] b)
         {
-            string path = AssetDatabase.GUIDToAssetPath(folder);
-            m_kSelectFolderPaths.Add(path);
-        }
-
-        AssetRefUtil.FindFolderRefs(m_kSelectFolderPaths.ToArray());
-
-        Repaint();
-    }
-
-
-    private void Update()
-    {
-
-        string[] currentGUIDs = Selection.assetGUIDs;
-        if (m_kLastGUIDs == null || !AreArraysEqual(m_kLastGUIDs, currentGUIDs))
-        {
-            m_kLastGUIDs = currentGUIDs;
-            _onSelectionChange();
-        }
-    }
-
-    private void OnGUI()
-    {
-        using (new GUILayout.HorizontalScope())
-        {
-            OnLeftPart();
-            OnRightPart();
-        }
-
-    }
-
-    private void OnRightPart()
-    {
-        using(new GUILayout.VerticalScope(GUILayout.ExpandWidth(true)))
-        {
-            if (m_kCurSelectUnityFileData == null)
-                return;
-
-            EditorGUILayout.LabelField($"Selected: {m_kCurSelectUnityFileData.m_kPath}");
-            EditorGUILayout.Space();
-            EditorGUILayout.LabelField($"Reference From:");
-
-            m_kScrollPos2 = GUILayout.BeginScrollView(m_kScrollPos2,EditorStyles.helpBox);
-
-            foreach (var refSrc in m_kCurSelectUnityFileData.m_kRefeds)
+            if (a == null || b == null || a.Length != b.Length) return false;
+            for (int i = 0; i < a.Length; i++)
             {
-                EditorGUILayout.LabelField(refSrc.m_kPath);
+                if (a[i] != b[i]) return false;
+            }
+            return true;
+        }
+
+
+        private void OnEnable()
+        {
+            if (m_kSearchField == null)
+            {
+                m_kSearchField = new SearchField();
             }
 
-            GUILayout.EndScrollView();
 
-            EditorGUILayout.Space();
-            EditorGUILayout.LabelField($"Depends:");
-            m_kScrollPos3 = GUILayout.BeginScrollView(m_kScrollPos3, EditorStyles.helpBox);
-            foreach (var dps in m_kCurSelectUnityFileData.m_kDepends)
-            {
-                EditorGUILayout.LabelField(dps.m_kPath);
-            }
-            GUILayout.EndScrollView();
-        }                           
-    }
-
-    private void OnLeftPart()
-    {
-
-        Vector2 windowSize = this.position.size;
-
-        GUILayout.BeginVertical(GUILayout.Width(windowSize.x/ 2));
-
-        foreach(var selectPath in m_kSelectFolderPaths)
-        {
-            EditorGUILayout.LabelField(selectPath);
+            EditorApplication.update -= Update;
+            EditorApplication.update += Update;
         }
 
-        m_kScrollPos1 = GUILayout.BeginScrollView(m_kScrollPos1,EditorStyles.helpBox,GUILayout.ExpandHeight(false));
-        int count1 = 0;
-        foreach (var selectData in AssetRefUtil.m_kSelectedUnityFileData)
+        private void OnDisable()
+        {
+            EditorApplication.update -= Update;
+        }
+
+
+        private void _onSelectionChange()
+        {
+            m_kCurSelectUnityFileData = null;
+            m_kRefFilePaths.Clear();
+            m_kSelectFolderPaths.Clear();
+
+            var curFolderGuids = Selection.assetGUIDs;
+            foreach (var folder in curFolderGuids)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(folder);
+                m_kSelectFolderPaths.Add(path);
+            }
+
+            AssetRefUtil.FindFolderRefs(m_kSelectFolderPaths.ToArray());
+
+            Repaint();
+        }
+
+
+        private void Update()
+        {
+
+            string[] currentGUIDs = Selection.assetGUIDs;
+            if (m_kLastGUIDs == null || !AreArraysEqual(m_kLastGUIDs, currentGUIDs))
+            {
+                m_kLastGUIDs = currentGUIDs;
+                _onSelectionChange();
+            }
+        }
+
+        private void OnGUI()
         {
             using (new GUILayout.HorizontalScope())
             {
-                if (GUILayout.Button($"{count1}", GUILayout.Width(m_fNumberWidth)))
-                {
-                    m_kCurSelectUnityFileData = selectData;
-                    GUIUtility.systemCopyBuffer = selectData.m_kPath;
-                }
-
-                EditorGUILayout.LabelField(selectData.m_kPath);
-                count1++;
+                OnLeftPart();
+                OnRightPart();
             }
 
         }
-        GUILayout.EndScrollView();
-        GUILayout.Space(10);
-        m_kCurSearchText = m_kSearchField.OnGUI(m_kCurSearchText);
 
-        m_kScrollPos = GUILayout.BeginScrollView(m_kScrollPos, EditorStyles.helpBox);
-        int count = 0;
-        foreach(var refData in AssetRefUtil.m_kRetDependsUnityFileData)
+        private void OnRightPart()
         {
-            if (m_kCurSearchText != null && m_kCurSearchText.Length > 0)
+            using (new GUILayout.VerticalScope(GUILayout.ExpandWidth(true)))
             {
-                if(refData.m_kPath.Contains(m_kCurSearchText))
+                if (m_kCurSelectUnityFileData == null)
+                    return;
+
+                EditorGUILayout.LabelField($"Selected: {m_kCurSelectUnityFileData.m_kPath}");
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField($"Reference From:");
+
+                m_kScrollPos2 = GUILayout.BeginScrollView(m_kScrollPos2, EditorStyles.helpBox);
+
+                foreach (var refSrc in m_kCurSelectUnityFileData.m_kRefeds)
+                {
+                    EditorGUILayout.LabelField(refSrc.m_kPath);
+                }
+
+                GUILayout.EndScrollView();
+
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField($"Depends:");
+                m_kScrollPos3 = GUILayout.BeginScrollView(m_kScrollPos3, EditorStyles.helpBox);
+                foreach (var dps in m_kCurSelectUnityFileData.m_kDepends)
+                {
+                    EditorGUILayout.LabelField(dps.m_kPath);
+                }
+                GUILayout.EndScrollView();
+            }
+        }
+
+        private void OnLeftPart()
+        {
+
+            Vector2 windowSize = this.position.size;
+
+            GUILayout.BeginVertical(GUILayout.Width(windowSize.x / 2));
+
+            foreach (var selectPath in m_kSelectFolderPaths)
+            {
+                EditorGUILayout.LabelField(selectPath);
+            }
+
+            m_kScrollPos1 = GUILayout.BeginScrollView(m_kScrollPos1, EditorStyles.helpBox, GUILayout.ExpandHeight(false));
+            int count1 = 0;
+            foreach (var selectData in AssetRefUtil.m_kSelectedUnityFileData)
+            {
+                using (new GUILayout.HorizontalScope())
+                {
+                    if (GUILayout.Button($"{count1}", GUILayout.Width(m_fNumberWidth)))
+                    {
+                        m_kCurSelectUnityFileData = selectData;
+                        GUIUtility.systemCopyBuffer = selectData.m_kPath;
+                    }
+
+                    EditorGUILayout.LabelField(selectData.m_kPath);
+                    count1++;
+                }
+
+            }
+            GUILayout.EndScrollView();
+            GUILayout.Space(10);
+            m_kCurSearchText = m_kSearchField.OnGUI(m_kCurSearchText);
+
+            m_kScrollPos = GUILayout.BeginScrollView(m_kScrollPos, EditorStyles.helpBox);
+            int count = 0;
+            foreach (var refData in AssetRefUtil.m_kRetDependsUnityFileData)
+            {
+                if (m_kCurSearchText != null && m_kCurSearchText.Length > 0)
+                {
+                    if (refData.m_kPath.Contains(m_kCurSearchText))
+                    {
+                        using (new GUILayout.HorizontalScope())
+                        {
+                            if (GUILayout.Button($"{count}", GUILayout.Width(m_fNumberWidth)))
+                            {
+                                m_kCurSelectUnityFileData = refData;
+                                GUIUtility.systemCopyBuffer = refData.m_kPath;
+                            }
+
+                            EditorGUILayout.LabelField(refData.m_kPath);
+                            count++;
+                        }
+                    }
+                }
+                else
                 {
                     using (new GUILayout.HorizontalScope())
                     {
-                        if( GUILayout.Button($"{count}",GUILayout.Width(m_fNumberWidth)))
+                        if (GUILayout.Button($"{count}", GUILayout.Width(m_fNumberWidth)))
                         {
                             m_kCurSelectUnityFileData = refData;
                             GUIUtility.systemCopyBuffer = refData.m_kPath;
                         }
-
                         EditorGUILayout.LabelField(refData.m_kPath);
                         count++;
                     }
                 }
             }
-            else
-            {
-                using (new GUILayout.HorizontalScope())
-                {
-                    if (GUILayout.Button($"{count}", GUILayout.Width(m_fNumberWidth)))
-                    {
-                        m_kCurSelectUnityFileData = refData;
-                        GUIUtility.systemCopyBuffer = refData.m_kPath;
-                    }
-                    EditorGUILayout.LabelField(refData.m_kPath);
-                    count++;
-                }
-            }
+
+            GUILayout.EndScrollView();
+
+            GUILayout.EndVertical();
         }
 
-        GUILayout.EndScrollView();
 
-        GUILayout.EndVertical();
+
+
     }
-
-
-
-
 }
+
